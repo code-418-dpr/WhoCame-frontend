3725c58ef3b06ab4572c55643b6b0cf050b8eb8d
V1ruses <103028077+Virus1es@users.noreply.github.com>
2025-02-17 00:30:41 +0300

    landing v1.0

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,70 +1,54 @@
-import Image from "next/image";
+import React from "react";
+import { platformFeatures } from "@/config/landing";
 
 export default function Home() {
     return (
-        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-            <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-                <Image className="dark:invert" src="/next.svg" alt="Next.js logo" width={180} height={38} priority />
-                <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-                    <li className="mb-2">
-                        Get started by editing{" "}
-                        <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-                            src/app/page.tsx
-                        </code>
-                        .
-                    </li>
-                    <li>Save and see your changes instantly.</li>
-                </ol>
-
-                <div className="flex gap-4 items-center flex-col sm:flex-row">
-                    <a
-                        className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-                        href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <Image className="dark:invert" src="/vercel.svg" alt="Vercel logomark" width={20} height={20} />
-                        Deploy now
-                    </a>
-                    <a
-                        className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-                        href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Read our docs
-                    </a>
-                </div>
-            </main>
-            <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-                <a
-                    className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-                    href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <Image aria-hidden src="/file.svg" alt="File icon" width={16} height={16} />
-                    Learn
-                </a>
-                <a
-                    className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-                    href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <Image aria-hidden src="/window.svg" alt="Window icon" width={16} height={16} />
-                    Examples
-                </a>
-                <a
-                    className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-                    href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <Image aria-hidden src="/globe.svg" alt="Globe icon" width={16} height={16} />
-                    Go to nextjs.org →
-                </a>
-            </footer>
-        </div>
+        <main className="space-y-6 pb-10 pt-52 lg:pt-48">
+            <div className="mx-auto w-full max-w-screen-xl">
+                <section className="flex flex-col items-center gap-10 text-center">
+                    <div>
+                        <h1 className="font-satoshi text-balance bg-gradient-to-r from-violet-600 via-blue-600 to-cyan-500 bg-clip-text text-[48px] font-black leading-[1.15] tracking-tight text-transparent sm:text-[56px] md:text-[80px] md:leading-[1.15]">
+                            WhoCame
+                        </h1>
+                        <h2 className="font-satoshi text-balance text-[48px] font-black leading-[1.15] tracking-tight sm:text-[56px] md:text-[64px] md:leading-[1.15]">
+                            Платформа для учёта посещаемости студентов
+                        </h2>
+                    </div>
+                    <p className="max-w-2xl text-balance text-lg text-muted-foreground sm:text-lg md:text-xl">
+                        Цель проекта — разработать систему с использованием искусственного
+                        интеллекта, которая распознает и позволит автоматически фиксировать
+                        время прихода, ухода студентов и время нахождения в учебном заведении,
+                        функциональный модуль минимизирует ручной труд, исключит
+                        человеческий фактор и оптимизирует посещаемость студентов учебных
+                        заведениях.
+                    </p>
+                </section>
+                <section className="container flex max-w-6xl flex-col gap-8 pt-32">
+                    <div className="flex flex-col items-center text-center">
+                        <div className="bg-gradient-to-r from-indigo-500 to-purple-500 bg-clip-text text-xl font-semibold text-transparent">
+                            Основные функции платформы
+                        </div>
+                    </div>
+                    <div className="column-1 gap-5 space-y-5 md:columns-2">
+                        {platformFeatures.map((feature, index) => (
+                            <div className="break-inside-avoid" key={index}>
+                                <div className="relative rounded-xl border bg-muted/25">
+                                    <div className="flex flex-col px-4 py-5 sm:p-6">
+                                        <div className="space-y-2">
+                                            <p className="font-semibold text-foreground">
+                                                {feature.title}
+                                            </p>
+                                            <p className="text-muted-foreground">
+                                                {feature.description}
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </section>
+            </div>
+        </main>
     );
 }
